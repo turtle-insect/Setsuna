f54660faca6e35f9fd1755e94c2f54049d69d80a
turtle-insect <turtle.insect@gmail.com>
2018-10-08 16:29:09 +0900

    Change My HomePage URL

--- Setsuna/AboutWindow.xaml.cs
+++ Setsuna/AboutWindow.xaml.cs
@@ -26,7 +26,7 @@ namespace Setsuna
 
 		private void LabelHP_MouseDown(object sender, MouseButtonEventArgs e)
 		{
-			System.Diagnostics.Process.Start("http://kamemushi.esy.es/");
+			System.Diagnostics.Process.Start("http://turtleinsect.php.xdomain.jp/");
 		}
 	}
 }
